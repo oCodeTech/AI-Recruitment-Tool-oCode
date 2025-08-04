c5d2fb46c243381bddf09eb58f730066dc771e3b
vivek singh <vivek@ocode.co>
2025-08-04 12:08:59 +0530

    chore: updated docker-compose file and removed redis connection tls param

--- src/queue/connection.ts
+++ src/queue/connection.ts
@@ -5,7 +5,6 @@ export const redisConnection: RedisOptions = {
   port: Number(process.env.REDIS_PORT),
   password: process.env.REDIS_PASSWORD,
   username: 'default',
-  tls: {},
 };
 
 export const redis = new Redis(redisConnection);

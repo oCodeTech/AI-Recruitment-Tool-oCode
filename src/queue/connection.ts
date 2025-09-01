3e2add5c9a81aac046bd62fe9109ff3ae7c4a20e
vivek singh <vivek@ocode.co>
2025-09-01 18:18:31 +0530

    fix: configured dev and prod redis connection

--- src/queue/connection.ts
+++ src/queue/connection.ts
@@ -1,9 +1,19 @@
 import { RedisOptions, Redis } from "ioredis";
 
 export const redisConnection: RedisOptions = {
-  host: process.env.REDIS_HOST,
-  port: Number(process.env.REDIS_PORT),
-  password: process.env.REDIS_PASSWORD,
+  host:
+    process.env.NODE_ENV === "development"
+      ? process.env.REDIS_DEV_HOST
+      : process.env.REDIS_HOST,
+  port: Number(
+    process.env.NODE_ENV === "development"
+      ? process.env.REDIS_DEV_PORT
+      : process.env.REDIS_PORT
+  ),
+  password:
+    process.env.NODE_ENV === "development"
+      ? process.env.REDIS_DEV_PASSWORD
+      : process.env.REDIS_PASSWORD,
   username: "default",
   // tls: {},
 };
